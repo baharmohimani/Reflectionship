bacac5e896c1543e8ee3e5f64901104f80972f35
= <=>
2016-10-27 17:48:07 -0700

    Started on second page, not finished yet.

--- routes/RelationshipProfileRoutes.js
+++ routes/RelationshipProfileRoutes.js
@@ -1,5 +1,11 @@
 /* Loads the relationship profile page. */
 
 exports.view = function (req, res) {
+    var ProfileInfo = {
+        "ProfileName": req.params.ProfileName,
+        "ProfileImage": "../images/ProfileImage.png",
+        "HomeButton": "../images/HomeSymbol.png"
+    };
 
+    res.render("RelationshipProfile", ProfileInfo);
 };
