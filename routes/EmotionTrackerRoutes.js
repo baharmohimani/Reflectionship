4377e5251f59d59486ea144acf1a10a153e3205d
Tara Nejad <tara919@gmail.com>
2016-11-07 13:05:33 -0800

    Added graphs

--- routes/EmotionTrackerRoutes.js
+++ routes/EmotionTrackerRoutes.js
@@ -7,3 +7,14 @@ exports.view = function (req, res) {
 
     res.render("EmotionTracker", EmotionInfo);
 };
+
+openGraph("Pie Chart");
+
+function openGraph(graphType) {
+    var i;
+    var x = document.getElementsByClassName("graph");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    document.getElementById(graphType).style.display = "block"; 
+}
