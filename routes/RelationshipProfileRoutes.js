756f9f9c525569071303e3f7cc2b5353c51bc5c9
Phillip Zhou <pzhou53@gmail.com>
2016-11-15 17:33:43 -0800

    Testing require for 'fs'.

--- routes/RelationshipProfileRoutes.js
+++ routes/RelationshipProfileRoutes.js
@@ -1,6 +1,6 @@
 /* Loads the relationship Profile page. */
 
-var Profile = require("../public/json/RelationshipProfile.json");
+// var Profile = require("../public/json/RelationshipProfile.json");
 var fs = require('fs');
 
 exports.profileInfo = function (req, res) {
