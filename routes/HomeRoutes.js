ba387507f115cf1663da54cb96c6f39d736173f5
Phillip Zhou <pzhou53@gmail.com>
2016-11-15 17:32:27 -0800

    Testing requires.

--- routes/HomeRoutes.js
+++ routes/HomeRoutes.js
@@ -31,7 +31,7 @@ exports.view = function (req, res) {
                         "ImageHolder": "images/ImageHolder.png",
                         "Image": "images/Relationship.png",
                         "ImageText": "images/RelationshipProfileText.png",
-                        "AspectLink": "/RelationshipProfile",
+                        "AspectLink": "/RelationshipProfile"
                         "ImageClass": "bottom-image",
                         "HolderClass": "bottom-holder"
                     },
