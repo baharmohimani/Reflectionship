bac6050cb9a7c8eb9179fbcc4cf0517073342d1a
Phillip Zhou <pzhou53@gmail.com>
2016-11-17 14:16:54 -0800

    2313123

--- routes/RelationshipProfileRoutes.js
+++ routes/RelationshipProfileRoutes.js
@@ -2,5 +2,18 @@
 
 exports.personal = function (req, res) {
     var Profile = require("./public/json/RelationshipProfile.json");
+
+    // First, grab the required data for all Profiles.
+    var HomeButton = Profile["HomeButton"];
+    var HomeLink = Profile["HomeLink"];
+
+    // Then, grab the specific Profile.
+    var ProfileID = Profile["UserID"];
+    var ProfileUsername = Profile["AllProfiles"][ProfileID]["Name"];
+    var Detail = Profile["AllProfiles"][ProfileID]["Info"];
+
+    // Combine the data together.
+    var ProfileData = { HomeButton, HomeLink, ProfileUsername, Detail };
+
     res.render("RelationshipProfile");
 };
