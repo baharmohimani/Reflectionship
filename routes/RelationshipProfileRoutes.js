f0528c88469b466e05f1dd0a55c46c9a2c891ffa
Phillip Zhou <pzhou53@gmail.com>
2016-11-17 17:36:00 -0800

    32343

--- routes/RelationshipProfileRoutes.js
+++ routes/RelationshipProfileRoutes.js
@@ -4,19 +4,7 @@ var ProfileInfo = require('../RelationshipProfile.json');
 var FileIO = require('fs');
 
 exports.getProfileInfo = function (req, res) {
-    // First, grab the required data for all Profiles.
-    var HomeButton = ProfileInfo["HomeButton"];
-    var HomeLink = ProfileInfo["HomeLink"];
-
-    // Then, grab the specific Profile.
-    var ProfileID = ProfileInfo["UserID"];
-    var ProfileUsername = ProfileInfo["AllProfiles"][ProfileID]["Name"];
-    var Detail = ProfileInfo["AllProfiles"][ProfileID]["Info"];
-
-    // Combine the data together.
-    var ProfileData = { HomeButton, HomeLink, ProfileUsername, Detail };
-
-    res.json(ProfileData);
+    res.json(ProfileInfo);
 }
 
 exports.personal = function (req, res) {
