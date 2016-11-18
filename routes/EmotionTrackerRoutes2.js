d41cd9316fbcbaaf018df4dc040c776374b62791
baharmohimani <br.1990m@gmail.com>
2016-11-17 18:59:24 -0800

    added the new page for emotiontracker2

---
+++ routes/EmotionTrackerRoutes2.js
@@ -0,0 +1,13 @@
+exports.view = function (req, res) {
+    var EmotionInfo = {
+        "AspectName": "Emotion Tracker",
+        "HomeButton": "../images/HomeSymbol.png",
+        "HomeLink": "/Home"
+
+          
+    
+};
+
+res.render("EmotionTracker2", EmotionInfo);
+};
+
