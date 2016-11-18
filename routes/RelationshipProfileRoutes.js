edb78bf4960581c8f0022c094111465aa0555b7c
Phillip Zhou <pzhou53@gmail.com>
2016-11-17 21:28:26 -0800

    Adding save call details

--- routes/RelationshipProfileRoutes.js
+++ routes/RelationshipProfileRoutes.js
@@ -12,5 +12,22 @@ exports.personal = function (req, res) {
 }
 
 exports.saveInfo = function (req, res) {
+    var ProfileID = parseInt(Profile["UserID"]);
+    var ProfileUser = Profile["AllProfiles"][ProfileID];
 
+    switch (req.body.detail) {
+        case '0':
+            ProfileUser["Info"]["TableBodyRows"] = JSON.parse(req.body.jsonStr);
+            break;
+        case '1':
+            ProfileUser["Likes"]["TableBodyRows"] = JSON.parse(req.body.jsonStr);
+            break;
+        case '2':
+            ProfileUser["Dislikes"]["TableBodyRows"] = JSON.parse(req.body.jsonStr);
+            break;
+        default:
+            break;
+    }
+
+    fs.writeFile("./public/json/RelationshipProfile.json", JSON.stringify(Profile), 'utf8');
 }
