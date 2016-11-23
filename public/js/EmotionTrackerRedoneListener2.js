80088a8c673eea8e333db0f9fd504918a62317e0
Phillip Zhou <pzhou53@gmail.com>
2016-11-23 08:54:03 -0800

    Redesigned the emotion tracker.

--- public/js/EmotionTrackerRedoneListener2.js
+++ public/js/EmotionTrackerRedoneListener2.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
 	initializePage();
 	
+    $("#emotion-list").hide();
+    $("#delete-btn").hide();
+    $("#emotion-list-blocker").hide();
 	$.get("/EmotionTrackerRedone2/Info", processInfo)
 })
 
@@ -12,10 +15,37 @@ function initializePage() {
 	// Setup listeners.
 	$("#record").click(saveEntry);
 	$("#reset").click(removeEntry);
+    $("#to-record").click(toRecord);
+    $("#to-log").click(toLog);
+    $("#question").click(showHelp);
+}
+
+function showHelp() {
+    window.alert("This is a log to track your emotions throughout your relationship with the currently selected person.");
+    console.log("HOY");
+}
+
+function toRecord() {
+    $("#emotion-bar").fadeIn();
+    $("#emotion-list").fadeOut();
+    $("#emotion-list-blocker").fadeOut();
+    $("#delete-btn").fadeOut();
+    $("#add-btn").fadeIn();
+}
+
+function toLog() {
+    $("#emotion-bar").fadeOut();
+    $("#emotion-list").fadeIn();
+    $("#emotion-list-blocker").fadeIn();
+    $("#delete-btn").fadeIn();
+    $("#add-btn").fadeOut();
+        
+    var objDiv = document.getElementById("emotion-list");
+    objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 function removeEntry(event) {
-	$("#emotion-list a").first().remove();
+	$("#emotion-list a").last().remove();
 
 	var SaveDataJSON = { jsonStr: "" };
 	$.post("/EmotionTracker/Save", SaveDataJSON, function(data) {
@@ -72,47 +102,30 @@ function saveEntry(event) {
 	var currDate = (month) + " " + (date.getDate());
 	
 	var details = $("#comment").val();
-
-	var properDate = "2016" + "-" + date.getMonth() + "-" + date.getDate();
-	//console.log(date.getYear());
-	//console.log(date.getMonth());
-	//console.log(date.getDate());
-	console.log(properDate);
-
 	
     // Finally, save data to JSON file.
     var SaveData = 
     {
     	Emotion: emotion,
     	Date: currDate,
-    	Details: details,
-    	properDate: properDate
+    	Details: details
 	};
 	
     var SaveDataJSON = { jsonStr: (JSON.stringify(SaveData)) };
-
-    $.post("/EmotionTracker/Save", SaveDataJSON);
-	$.get("/EmotionTrackerRedone2/Info", processInfo);
-}
-/*
- * GET home page.
- */
-function keysrt(key, desc) {
-    return function (a, b) {
-        return desc ? ~~(a[key] < b[key]) : ~~(a[key] > b[key]);
-    }
+    
+    $.post("/EmotionTracker/Save", SaveDataJSON, function(data) {
+		$.get("/EmotionTrackerRedone2/Info", processInfo);
+	});
 }
 
 function processInfo(result) {
     var UserID = parseInt(result["UserID"]);
     var User = result["AllProfiles"][UserID];
     $("#name-text").text("Emotion Entry For: " + User["Name"]);
-
+    
+    console.log("Hello");
     var EmotionHTML = "";  // Holds all of the HTML code for the emotionlist.
 
-    //console.log(User["Entries"]);
-    User["Entries"].sort(keysrt('properDate', false));
-    //console.log(User["Entries"]);
     for (var i = 0; i < User["Entries"].length; i++) {
         var entry = User["Entries"][i];
 		var color = null;
